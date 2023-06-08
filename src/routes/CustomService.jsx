80e6e4c796441d5792f2e809a4822dc78555a152
SachinSignphic <135112237+SachinSignphic@users.noreply.github.com>
2023-06-08 09:53:14 +0530

    Update CustomService.jsx

--- src/routes/CustomService.jsx
+++ src/routes/CustomService.jsx
@@ -29,6 +29,28 @@ const CustomService = () => {
           </p>
         }   
       />
+      <ServiceSectionRTL 
+        heading="Branding" 
+        whiteText="Our" 
+        purpleText="Pack Builder" 
+        pMarkup={
+          <p className="ff-gillroy-l fs-2m sec-color">
+            We&lsquo;ll refine this list and your quote once we receive the order and provide suggestions of how you can best use your resources.
+                1st things first, your brand. Whether you&lsquo;re looking for a new logo design or a full brand guide we have you covered.
+          </p>
+        }   
+      />
+      <ServiceSectionLTR 
+        heading="Branding" 
+        whiteText="Our" 
+        purpleText="Pack Builder" 
+        pMarkup={
+          <p className="ff-gillroy-l fs-2m sec-color">
+            We&lsquo;ll refine this list and your quote once we receive the order and provide suggestions of how you can best use your resources.
+                1st things first, your brand. Whether you&lsquo;re looking for a new logo design or a full brand guide we have you covered.
+          </p>
+        }   
+      />
       {/* <ServiceSectionLTR /> */}
       {/* <ServiceSectionRTL /> */}
       {/* <ServiceSectionLTR /> */}
