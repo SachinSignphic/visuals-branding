61f2da6cba4645eace545739a5f725c57b8c6b7d
SachinSignphic <135112237+SachinSignphic@users.noreply.github.com>
2023-06-19 16:00:56 +0530

    Add files via upload

--- src/routes/CustomService.jsx
+++ src/routes/CustomService.jsx
@@ -16,8 +16,12 @@ const CustomService = () => {
         purpleText="Pack Builder" 
         pMarkup={
           <p className="ff-gillroy-l fs-2m sec-color">
-            We&lsquo;ll refine this list and your quote once we receive the order and provide suggestions of how you can best use your resources.
-                <br /><br />1st things first, your brand. Whether you&lsquo;re looking for a new logo design or a full brand guide we have you covered.
+           Build your identity alongside our brand experts. We’ll design a cohesive, one of a kind look that’s inspired by your unique personality, values and audience. <br /><br />The #1 starting place for creators seeking a professional rebrand - or building their first live- streaming identity. Your Visuals Branding kit will include :
+<br /><br />
+Logo Design, 
+Color palette creation, 
+Typography selection, 
+Brand guidelines, including logo usage & visual language
           </p>
         }   
       />
@@ -27,9 +31,17 @@ const CustomService = () => {
         purpleText="Overlays" 
         pMarkup={
           <p className="ff-gillroy-l fs-2m sec-color">
-            We&lsquo;ll refine this list and your quote once we receive the order and provide suggestions of how you can best use your resources.
-                <br /><br />1st things first, your brand. Whether you&lsquo;re looking for a new logo design or a full brand guide we have you covered.
-          </p>
+           The full package for live performers. We’ll overhaul your complete broadcast system — or create one from scratch — to design a custom production that <span className="blk-color ff-almeria-b">reflects your brand.</span>
+            <br/ ><br />
+            Our most popular option for streamers, gaming creators, podcasters, virtual DJs and showrunners.<span className="blk-color ff-almeria-b">Visuals Branding</span> stream overlay packages typically include:
+            <br/ ><br />
+            Modular live overlays
+            <br />
+            3D motion design
+            <br />
+            Widgets & alerts
+            <br />
+            Screens & profile art          </p>
         }   
       />
       <ServiceSectionRTL 
@@ -38,8 +50,16 @@ const CustomService = () => {
         purpleText="Widgets" 
         pMarkup={
           <p className="ff-gillroy-l fs-2m sec-color">
-            We&lsquo;ll refine this list and your quote once we receive the order and provide suggestions of how you can best use your resources.
-                <br /><br />1st things first, your brand. Whether you&lsquo;re looking for a new logo design or a full brand guide we have you covered.
+           {/* <span className="blk-color ff-almeria-b">Build your identity</span> alongside our brand experts. */}
+           Push the limits of broadcast innovation. The synergy between designer and developer is what sets our team apart. Enlist our creative engineers to start custom-building for your channel — no challenge is too large.
+            <br /><br />
+            <span className="blk-color ff-almeria-b">Visuals Branding</span> developers apply advanced coding to build interactive overlays and alert systems. Their work spans across all live-streaming software, including:
+            <br /><br />
+            Streamlabs
+            <br />
+            StreamElements
+            <br />
+            OBS Studio
           </p>
         }   
       />
@@ -49,8 +69,61 @@ const CustomService = () => {
         purpleText="Motion Package" 
         pMarkup={
           <p className="ff-gillroy-l fs-2m sec-color">
-            We&lsquo;ll refine this list and your quote once we receive the order and provide suggestions of how you can best use your resources.
-                <br /><br />1st things first, your brand. Whether you&lsquo;re looking for a new logo design or a full brand guide we have you covered.
+            {/* <span className="blk-color ff-almeria-b">Build your identity</span> alongside our brand experts. */}
+            Need a specialty animation for an upcoming broadcast or event? Commission our motion designers for an independent project — we’re experts in advanced motion designgin & animation work.
+            <br /><br />
+            <span className="blk-color ff-almeria-b">Visuals Branding</span> works with creators, brands, esports orgs and game publishers on a variety of motion projects, including:
+            <br /><br />
+            Intros & outros
+            <br />
+            Screens, alerts & stinger transitions
+            <br />
+            Lower thirds
+            <br />
+            Promotional assets & more
+          </p>
+        }   
+      />
+      <ServiceSectionRTL 
+        heading="Emotes and Badges" 
+        whiteText="Our" 
+        purpleText="Emotes" 
+        pMarkup={
+          <p className="ff-gillroy-l fs-2m sec-color">
+            {/* <span className="blk-color ff-almeria-b">Build your identity</span> alongside our brand experts. */}
+            Reward your fans with custom chat experiences. Recruit our team of illustrators — we’ll hand-draw personalized emotes and badges for your channel.
+            <br /><br />
+            <span className="blk-color ff-almeria-b">Visuals Branding</span> is home to some of the industry’s most accomplished pixel artists. Together, we’ve created tens of thousands of custom emoticons, including:
+            <br /><br />
+            Twitch emotes
+            <br />
+            YouTube Super Stickers
+            <br />
+            Facebook Gaming badges
+            <br />
+            Discord animated emotes
+          </p>
+        }   
+      />
+      <ServiceSectionLTR 
+        heading="3d Environment Creation" 
+        whiteText="Our" 
+        purpleText="3d Environment" 
+        pMarkup={
+          <p className="ff-gillroy-l fs-2m sec-color">
+            <span className="blk-color ff-almeria-b">Build your identity</span> alongside our brand experts.
+            We’ll design a cohesive, one of a kind look that’s
+            inspired by your unique personality, values and
+            audience. <br /><br />
+            The <span className="blk-color ff-almeria-b">#1</span> starting place for creators seeking a
+            professional rebrand - or building their first live-
+            streaming identity. Your <span className="blk-color ff-almeria-b">Visuals Branding </span>kit will
+            include :<br /><br />
+            Logo Design<br />
+            Color palette creation<br />
+            Typography selection<br />
+            Brand guidelines, including logo usage & visual
+            language
           </p>
         }   
       />
