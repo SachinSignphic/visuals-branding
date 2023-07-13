d91a90af15aed33dad9fbe4fecd6c1b4342e5ec0
SachinSignphic <135112237+SachinSignphic@users.noreply.github.com>
2023-07-13 06:42:15 +0530

    Add files via upload

--- src/components/FadingBackground/FadingBackground.jsx
+++ src/components/FadingBackground/FadingBackground.jsx
@@ -8,11 +8,12 @@ const FadingBackground = () => {
     useEffect(() => {
       const rootElem = document.getElementById("bg");
       const handleScroll = () => {
-        var opValue = 0.6 - scale(window.scrollY, 0, windowHeight, 0, 0.6)*1.8
+        var opValue = 0.9 - scale(window.scrollY, 0, windowHeight, 0, 0.9)*1.8
         rootElem.style.setProperty("--bg-opacity", (opValue>0)? opValue : 0);
         if (opValue <= 0) return; 
-        // console.log(window.scrollY, opValue);
+        // console.log(window.scrollY);
       }
+      // console.log(window.scrollY);
       window.addEventListener("scroll", handleScroll);
       return () => { window.removeEventListener("scroll", handleScroll); }
     },[windowHeight]);   
